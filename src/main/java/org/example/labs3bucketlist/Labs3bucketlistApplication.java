fc45e9ff777d8601fc4ea9079e0f1979fd19bb55
August Borse Haraldsen <144022756+BorseHaraldsen@users.noreply.github.com>
2024-11-04 22:20:08 +0100

    first commit

---
+++ src/main/java/org/example/labs3bucketlist/Labs3bucketlistApplication.java
@@ -0,0 +1,13 @@
+package org.example.labs3bucketlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labs3bucketlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labs3bucketlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/labs3bucketlist/Labs3bucketlistApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.labs3bucketlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labs3bucketlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
